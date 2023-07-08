dc9067aa00ea27fb95628f2889596604828d5bbc
Sacha Barragan <sacha.barragan.external@capgemini.com>
2023-07-08 20:16:35 -0300

    Creating Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import UserList from './Employees/UserList';
+import CreateEmployee from "./Employees/CreateEmployee";
+import EditEmployee from "./Employees/EditEmployee";
+import DeleteEmployee from "./Employees/DeleteEmployee";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>CRUD REACT App</h1>
-      <UserList/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='' element={<UserList />} />
+          <Route path='/crear-empleado' element={<CreateEmployee />} />
+          <Route path='/editar-empleado' element={<EditEmployee />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Employees/UserList.js
+++ frontend/src/Employees/UserList.js
@@ -1,16 +1,11 @@
 import { React } from "react";
 import OneEmployee from "./OneEmployee";
-import CreateEmployee from "./CreateEmployee";
-import EditEmployee from "./EditEmployee";
-import DeleteEmployee from "./DeleteEmployee";
+
 
 function UserList(){
     return (<div>
         <h2>Component EmployeeList</h2>
         <OneEmployee/>
-        <CreateEmployee/>
-        <EditEmployee/>
-        <DeleteEmployee/>
     </div>)
 }
 
