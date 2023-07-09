8095375a1a88d6cca1027fe633fadbb449b46549
Sacha Barragan <sacha.barragan.external@capgemini.com>
2023-07-09 19:06:24 -0300

    Adding mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
+const archivoDB = require('./connection'); 
 
 app.get('/', (req, res) => {
-    res.send('¡Hola, perro!');
+  res.send('¡Hola, perro!');
 });
 
 app.listen(5000, () => {
-  console.log('Servidor piola  Express iniciado en el puerto 3000');
+  console.log('Servidor piola Express iniciado en el puerto 5000');
 });
-
-
